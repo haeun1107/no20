3072f9b9e5011eb6a7004af2a1e042d64d9b548a
haeun1107 <haeun9394@gmail.com>
2024-12-09 17:51:19 +0900

    Feat: health check용 API 작성

--- src/main/java/dgu/notwenty/global/base/RootController.java
+++ src/main/java/dgu/notwenty/global/base/RootController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
 
+    @GetMapping("/")
+    public String healthCheck() {
+        return "OK";  // 혹은 상태에 따라 적절한 메시지를 반환
+    }
 }
